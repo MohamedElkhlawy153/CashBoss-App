ee907d15c0ec6ac1600b69ca88d51ec5b96943d5
Mohamed Mahmoud <133065999+MohamedElkhlawy153@users.noreply.github.com>
2025-10-29 01:44:08 +0300

    add function to initialize the database and create the transactions table if it doesn't exist

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,3 +6,22 @@ dotenv.config();
 // create a sql connection using our DATABASE_URL from .env file
 export const sql = neon(process.env.DATABASE_URL);
 
+
+// function to initialize the database and create the transactions table if it doesn't exist
+export async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+        console.log("Database initialized successfully and table ensured.");
+    } catch (error) {
+        console.error("Error initializing database:", error);
+        process.exit(1); // status code 1 indicates failure and 0 success
+    }
+}
