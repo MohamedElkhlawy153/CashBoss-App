af143e3269d9355b5f35cd14fe8a44109c43c392
Mohamed Mahmoud <133065999+MohamedElkhlawy153@users.noreply.github.com>
2025-10-30 21:37:49 +0300

    A wrapper component to provide safe area padding and consistent background color

---
+++ mobile/components/SafeScreen.jsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import {COLORS} from '../constants/colors'
+
+// A wrapper component to provide safe area padding and consistent background color
+const SafeScreen = ({children}) => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{flex:1, paddingTop: insets.top, backgroundColor:COLORS.background}}>
+        {children}
+    </View>
+  )
+}
+
+export default SafeScreen
