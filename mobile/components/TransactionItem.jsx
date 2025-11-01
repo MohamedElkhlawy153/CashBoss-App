c2c1d94b3ace70351476da5a6f7fe6b5eb2f0e5d
Mohamed Mahmoud <133065999+MohamedElkhlawy153@users.noreply.github.com>
2025-11-01 03:35:14 +0200

    create Create Transaction Page

--- mobile/components/TransactionItem.jsx
+++ mobile/components/TransactionItem.jsx
@@ -34,7 +34,7 @@ export const TransactionItem = ({ item, onDelete }) => {
           <Text
             style={[styles.transactionAmount, { color: isIncome ? COLORS.income : COLORS.expense }]}
           >
-            {isIncome ? "+" : "-"}${Math.abs(parseFloat(item.amount)).toFixed(2)}
+            {isIncome ? "+" : "-"}E£{Math.abs(parseFloat(item.amount)).toFixed(2)}
           </Text>
           <Text style={styles.transactionDate}>{formatDate(item.created_at)}</Text>
         </View>
